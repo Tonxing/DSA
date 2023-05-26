dae1aed0660e6801710f9044aff0ae8f21bd05f3
MM Tonsing <langmuanmungtonxing@gmail.com>
2023-05-26 20:47:36 +0530

    added many more features on array like reverse, Shift, Rotate, Sort array, Merge

--- ArrayADT.c
+++ ArrayADT.c
@@ -9,8 +9,10 @@ struct Array
 };
 
 struct Array arr;
+struct Array newarr;
+struct Array *mergedarr;
 
-int i;
+int i, j, k;
 
 void Display(struct Array arr)
 {
@@ -139,9 +141,168 @@ float Avg(struct Array arr)
     return (float)total / arr.length;
 }
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void ReverseUsingDublicate(struct Array *arr)
+{
+    int *B;
+    B = (int *)malloc(arr->length * sizeof(int));
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    for (i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+}
+
+void ReverseUsingSwap(struct Array *arr)
+{
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+        swap(&arr->A[i], &arr->A[j]);
+}
+
+void LeftShift(struct Array *arr)
+{
+    for (i = 0; i < arr->length - 1; i++)
+    {
+        arr->A[i] = arr->A[i + 1];
+    }
+    arr->A[arr->length - 1] = 0;
+}
+
+void LeftRotate(struct Array *arr)
+{
+    int x = arr->A[0];
+    for (i = 0; i < arr->length - 1; i++)
+    {
+        arr->A[i] = arr->A[i + 1];
+    }
+    arr->A[arr->length - 1] = x;
+}
+
+void RightShift(struct Array *arr)
+{
+    for (i = arr->length - 1; i > 0; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
+    arr->A[0] = 0;
+}
+
+void RightRotate(struct Array *arr)
+{
+    int x = arr->A[arr->length - 1];
+    for (i = arr->length - 1; i > 0; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
+    arr->A[0] = x;
+}
+
+int isSorted(struct Array arr)
+{
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void SortArray(struct Array *arr)
+{
+    for (i = 0; i < arr->length - 1; i++)
+    {
+        for (j = 0; j < arr->length - i - 1; j++)
+        {
+            if (arr->A[j] > arr->A[j + 1])
+            {
+                swap(&arr->A[j], &arr->A[j + 1]);
+            }
+        }
+    }
+}
+
+void SortnInsert(struct Array *arr, int x)
+{
+    if (arr->length == arr->size)
+    {
+        printf("Array is already full.");
+        return;
+    }
+
+    if (isSorted(*arr) == 0)
+    {
+        SortArray(arr);
+    }
+
+    i = arr->length - 1;
+    while (arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+void Separation(struct Array *arr)
+{
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+            i++;
+        while (arr->A[j] >= 0)
+            j--;
+        if (i < j)
+            swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+struct Array *Merge(struct Array *arr, struct Array *newarr)
+{
+    i = 0, j = 0, k = 0;
+
+    struct Array *mergedarr = (struct Array *)malloc(sizeof(struct Array));
+    printf("Enter size of the merged array: ");
+    scanf("%d", &mergedarr->size);
+
+    if (isSorted(*arr) == 0)
+    {
+        SortArray(arr);
+    }
+
+    if (isSorted(*newarr) == 0)
+    {
+        SortArray(newarr);
+    }
+
+    while (i < arr->length && j < newarr->length)
+    {
+        if (arr->A[i] < newarr->A[j])
+            mergedarr->A[k++] = arr->A[i++];
+        else
+            mergedarr->A[k++] = newarr->A[j++];
+    }
+    for (; i < arr->length; i++)
+        mergedarr->A[k++] = arr->A[i];
+    for (; j < newarr->length; j++)
+        mergedarr->A[k++] = newarr->A[j];
+    mergedarr->length = arr->length + newarr->length;
+}
+
 int main()
 {
-    int n, s, element, index;
+    int n, m, s, element, index;
     char choice;
 
     printf("Enter size of an array: ");
@@ -173,6 +334,16 @@ int main()
         printf("\n10. Get minimum element.");
         printf("\n11. Get sum of all elements.");
         printf("\n12. Get average.");
+        printf("\n13. Reverse using dublicate array.");
+        printf("\n14. Reverse using swapping.");
+        printf("\n15. Left Shift.");
+        printf("\n16. Left Rotate.");
+        printf("\n17. Right Shift.");
+        printf("\n18. Right Rotate.");
+        printf("\n19. Check if elements are sorted.");
+        printf("\n20. Insert element by sorting it first if not yet sorted.");
+        printf("\n21. Separate/Group negative from positive elements.");
+        printf("\n22. Merge Array.");
 
         printf("\nEnter choice: ");
         scanf("%d", &s);
@@ -250,6 +421,69 @@ int main()
 
         case 12:
             printf("Average of all the elements: %.2f", Avg(arr));
+            break;
+
+        case 13:
+            ReverseUsingDublicate(&arr);
+            break;
+
+        case 14:
+            ReverseUsingSwap(&arr);
+            break;
+
+        case 15:
+            LeftShift(&arr);
+            break;
+
+        case 16:
+            LeftRotate(&arr);
+            break;
+
+        case 17:
+            RightShift(&arr);
+            break;
+
+        case 18:
+            RightRotate(&arr);
+            break;
+
+        case 19:
+            if (isSorted(arr) == 1)
+                printf("The elements are sorted.");
+            else
+                printf("The elements are not sorted.");
+            break;
+
+        case 20:
+            printf("Enter the element to insert: ");
+            scanf("%d", &element);
+            SortnInsert(&arr, element);
+            break;
+
+        case 21:
+            Separation(&arr);
+            break;
+
+        case 22:
+
+            printf("For th second array:\n");
+            printf("Enter size of the array: ");
+            scanf("%d", &newarr.size);
+            newarr.A = (int *)malloc(newarr.size * sizeof(int));
+            newarr.length = 0;
+
+            printf("Enter length: ");
+            scanf("%d", &m);
+
+            printf("Enter all Elements\n");
+            for (i = 0; i < m; i++)
+            {
+                scanf("%d", &newarr.A[i]);
+            }
+            newarr.length = m;
+
+            mergedarr = Merge(&arr, &newarr);
+            Display(*mergedarr);
             break;
 
         default:
